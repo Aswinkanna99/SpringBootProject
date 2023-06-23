8832287697415ad2a0ca0746c6f7c6a4f6550da8
Aswinkanna99 <aswinkannagit2019@citchennai.net>
2023-06-24 01:01:08 +0530

    first commit

---
+++ src/main/java/com/pack/SpringBoot1/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.pack.SpringBoot1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloController {
+	
+    @GetMapping("/hello")
+	public String getMessage()
+	{
+		return "Hello World";
+	}
+}
